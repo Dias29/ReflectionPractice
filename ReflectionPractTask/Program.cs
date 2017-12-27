601e881ffc470ccbac7c20c3bc2cf71e5b621551
УринбасаровД <@A-305-10>
2017-12-27 18:48:19 +0600

    first

---
+++ ReflectionPractTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionPractTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str= "Не ищите идеального человека. Сегодня я дома. (с) А. Челентано";
+
+            Type type = typeof(String);
+            
+            MethodInfo method = type.GetMethod("Substring", new Type[] {typeof(int), typeof(int)});
+
+            object methodValue = method.Invoke(str, new object[] { 30, 14 });
+
+            Console.WriteLine(methodValue);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type list = typeof(List<>);
+            Type[] types = new Type[1];
+            types[0] = typeof(object);
+
+            ConstructorInfo info = list.GetConstructor(new Type[] {typeof(object)});
+
+            Console.WriteLine("Конструкторы класса List<object> : " + info.ToString());
+            Console.ReadLine();
+        }
+    }
+}
